4e0bdd768464cfae2844b937626f1575eeb4e6bc
Daan van der Zwaag <daan@devign.it>
2021-09-23 12:19:14 -0400

    fix: add correct URL to video in footer

--- components/Footer.js
+++ components/Footer.js
@@ -12,8 +12,12 @@ const Footer = () => {
         Project by Daan van der Zwaag, 2021
       </a>
       <div>
-        <a href="#m" target="_blank" rel="noopener noreferrer">
-          View video on Youtube<span>↗</span>
+        <a
+          href="https://www.loom.com/share/a1711a6461d34537ac8b4cc08175edb5"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View video on Loom<span>↗</span>
         </a>
         <a
           href="https://github.com/danoszz/cornell-info5410-urban-systems_dv239"
